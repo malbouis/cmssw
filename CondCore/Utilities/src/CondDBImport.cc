d5720b73029fd2d88c95897f0324e2ba44b1deff
Helena <helena.malbouisson@gmail.com>
2016-07-07 19:38:11 +0200

    add CTPPSDAQMapping

--- CondCore/Utilities/src/CondDBImport.cc
+++ CondCore/Utilities/src/CondDBImport.cc
@@ -73,6 +73,7 @@ namespace cond {
       IMPORT_PAYLOAD_CASE( CastorRecoParams )
       IMPORT_PAYLOAD_CASE( CastorSaturationCorrs )
       IMPORT_PAYLOAD_CASE( CentralityTable )
+      IMPORT_PAYLOAD_CASE( CTPPSDAQMapping )
       IMPORT_PAYLOAD_CASE( DTCCBConfig )
       IMPORT_PAYLOAD_CASE( DTDeadFlag )
       IMPORT_PAYLOAD_CASE( DTHVStatus )
