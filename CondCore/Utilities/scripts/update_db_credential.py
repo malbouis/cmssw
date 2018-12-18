005a596a569c093c502d57b30a016890da50f323
Helena <helena.malbouisson@gmail.com>
2018-12-18 18:27:51 +0100

    include missing alignment DB files

--- CondCore/Utilities/scripts/update_db_credential.py
+++ CondCore/Utilities/scripts/update_db_credential.py
@@ -4,13 +4,8 @@ import getpass
 import subprocess
 import optparse
 
-def update_credential( serviceName, accountName, label, newPassword ):
-    command = 'cmscond_authentication_manager --update_conn -s %s -u %s -p %s'
-    params = (serviceName, accountName, newPassword )
-    if label != '-':
-        command += ' -l %s'
-        params += ( label, )
-    command = command %params
+def update_credential( serviceName, accountName, newPassword ):
+    command = 'cmscond_authentication_manager --update_conn -s %s -u %s -p %s' %(serviceName, accountName, newPassword )
     pipe = subprocess.Popen( command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
     stdout_val = pipe.communicate()[0]
     return stdout_val
@@ -31,12 +26,6 @@ def main():
         help = 'the account name to change'
     )
 
-    parser.add_option('-l','--label',
-        dest = 'label',
-        default = '-',
-        help = 'the connection label. default=accountName@service'
-    )
-
     (options, arguments) = parser.parse_args()
   
     if options.service == None or options.accountName == None:
@@ -46,9 +35,9 @@ def main():
     password = getpass.getpass( prompt= 'Enter the new password:')
 
     try:
-        print update_credential( options.service, options.accountName, options.label, password )
-        print 'Credentials updated.' 
-    except Exception as e:
-        print 'Update credential failed: %s'%str(e) 
+        print update_credential( options.service, options.accountName, password )
+        print 'Password changed.' 
+    except e:
+        print 'Update credential failed.' 
 if __name__ == '__main__':
     sys.exit(main())
