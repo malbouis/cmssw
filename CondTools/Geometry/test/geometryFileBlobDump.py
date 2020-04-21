e4144255fc59f35756c01634bbb69a96c38dbc16
Helena <helena.malbouisson@gmail.com>
2020-04-21 22:43:18 +0200

    PPS SIM geometry xml FileBlob 	CondTools/Geometry/test/writehelpers/geometryExtended2018_PPS_xmlwriter.py 	CondTools/Geometry/test/writehelpers/mc_ppsgeometrywriter.py 	CondTools/Geometry/test/writehelpers/ppsgeometry_xmlwriter.py

--- CondTools/Geometry/test/geometryFileBlobDump.py
+++ CondTools/Geometry/test/geometryFileBlobDump.py
@@ -5,11 +5,12 @@ process = cms.Process("GeometryFileDump")
 process.load('CondCore.CondDB.CondDB_cfi')
 process.load('Configuration.StandardSequences.GeometryDB_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = autoCond['run1_mc']
+process.GlobalTag.globaltag = autoCond['run2_mc']
 process.GlobalTag.toGet = cms.VPSet(
     cms.PSet(record = cms.string("GeometryFileRcd"),
-             tag = cms.string("XMLFILE_Geometry_81YV14_Extended2017_mc"),
-             connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+             tag = cms.string("XMLFILE_Geometry_TagV1_Extended2018_PPS_mc"),
+             #connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+             connect = cms.string("sqlite_file:cmsGeometryExtended2018_PPS.db")
              # label = cms.string("Extended")
              )
     )
@@ -22,7 +23,7 @@ process.source = cms.Source("EmptyIOVSource",
                             )
 
 process.GeometryFileDump = cms.EDAnalyzer("XMLGeometryReader",
-                                          XMLFileName = cms.untracked.string("GeometryExtended2017.81YV14.xml"),
+                                          XMLFileName = cms.untracked.string("XMLFILE_Geometry_TagV1_ExtendedPPS2018_mc.xml"),
                                           geomLabel = cms.untracked.string("")
                                           )
 
